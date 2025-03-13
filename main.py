d22664f99483fa4324efb1d9a800acef21440f7d
ionlyhavethisfor <56805393+ionlyhavethisfor@users.noreply.github.com>
2025-03-13 16:07:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,5 +49,5 @@ def update_hnpcarousel_text(indeks):
         return carousel_desc_list[indeks]
     return carousel_desc_list[0]
 # if __name__ == "__main__":
-app.run_server()
+app.run_server(host='0.0.0.0', port=int(os.getenv('PORT', 8050)))
 
