4c3f78ca72a51b8edede7c0425402de22cac7e7c
ionlyhavethisfor <56805393+ionlyhavethisfor@users.noreply.github.com>
2025-03-13 15:42:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 15:13:21 2025
+
+@author: caspe
+"""
+
+from dbc_components import *
+from flask_caching import Cache
+import dash_bootstrap_components as dbc
+from dash import Dash, html, Input, Output, dcc, State, ctx, ALL, no_update, dash_table
+import dash
+
+
+
+# %%
+
+app = Dash(__name__, use_pages=True, pages_folder='pages',
+           external_stylesheets=[dbc.themes.JOURNAL, dbc.icons.BOOTSTRAP],
+           meta_tags=[
+               {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+           ],
+           )
+server = app.server
+
+app.layout = dbc.Container([
+    breadcrumbs,
+    # html.Div([dcc.Link(page['name']+" | ", href=page['path']) for page in dash.page_registry.values()]),
+
+    dash.page_container
+    ], class_name="container", fluid=True)
+
+
+# @app.callback(
+#     Output('hnp_text', 'children'),
+#     Input('hnp_carousel', 'active_index')
+# )
+# def update_hnpcarousel_text(indeks):
+#     if indeks:
+#         return carousel_desc_list[indeks]
+#     return carousel_desc_list[0]
+
+@dash.callback(
+    Output('hnp_text', 'children'),
+    Input('hnp_carousel', 'active_index')
+)
+def update_hnpcarousel_text(indeks):
+    if indeks:
+        return carousel_desc_list[indeks]
+    return carousel_desc_list[0]
+# if __name__ == "__main__":
+app.run_server(debug=True, use_reloader=False)
+
