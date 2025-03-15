e3f9477f971241996b88cf9bea8bd48b2d721216
ionlyhavethisfor <56805393+ionlyhavethisfor@users.noreply.github.com>
2025-03-15 18:32:43 +0100

    Update pg3.py

--- pages/pg3.py
+++ pages/pg3.py
@@ -12,13 +12,30 @@ from dbc_components import *
 
 # Page layout for pg3
 layout = html.Div([
-    dbc.Row([
-        hnp_carousel, 
-        html.P("", id="hnp_text")
-    ], class_name="row_block_class", id="hnp"),
+    dbc.Tabs([
+        dbc.Tab(
+            dbc.Row([
+                hnp_carousel, 
+                html.P("", id="hnp_text")
+            ], class_name="row_block_class", id="hnp"),
+            
+            label="Gallery", tab_id='gallery'),
+        dbc.Tab(
+            dbc.Row([
+                html.Iframe(
+                    src="https://www.youtube.com/embed/9tXKMm2DL68",
+                    style={"width": "100%", "height": "auto", 'padding': '0 10vh 20vh 10vh'}
+                ),
+                ], class_name="row_block_class", id='video'),
+            
+            label="Video"),
+    ], id="tabs", active_tab="gallery"),
+
+
 ])
 
 # Register the page with a specific path
 dash.register_page(__name__)
+
 
 
