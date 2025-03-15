8167f6b4a77dc1ca93e66f02c34524c64f95d8a1
ionlyhavethisfor <56805393+ionlyhavethisfor@users.noreply.github.com>
2025-03-15 18:31:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ import dash_bootstrap_components as dbc
 from dash import Dash, html, Input, Output, dcc, State, ctx, ALL, no_update, dash_table
 import dash
 
+color_mode_switch =  dbc.Stack([
+    html.I(className="bi bi-brightness-high"),
+    html.Span(
+    [
+        dbc.Label(className="fa fa-moon", html_for="switch"),
+        dbc.Switch( id="switch", value=True, className="d-inline-block ms-1", persistence=True),
+        dbc.Label(className="fa fa-sun", html_for="switch"),
+    ])
+    
+    ], direction='horizontal', style={"position": "absolute", 'right': "5vw", 'top': "5vh"})
+
+breadcrumb_pages = [{"label": page_names[idx], "href": page['path']} for idx, page in enumerate(dash.page_registry.values())]
 
 
 # %%
@@ -22,30 +34,27 @@ app = Dash(__name__, use_pages=True, pages_folder='pages',
            ],
            )
 server = app.server
-
 app.layout = dbc.Container([
+
     dbc.Breadcrumb(
-    items=[
-        {"label": page_names[idx], "href": page['path']}
-        for idx, page in enumerate(dash.page_registry.values())
-    ],
-    class_name="navbar",
-    
-    ),
-    # html.Div([dcc.Link(page['name']+" | ", href=page['path']) for page in dash.page_registry.values()]),
+        items=breadcrumb_pages,
+        class_name="navbar",
+        id="breadcrumb"
+        ),
+    color_mode_switch,
 
     dash.page_container
     ], class_name="container", fluid=True)
-
-
-# @app.callback(
-#     Output('hnp_text', 'children'),
-#     Input('hnp_carousel', 'active_index')
-# )
-# def update_hnpcarousel_text(indeks):
-#     if indeks:
-#         return carousel_desc_list[indeks]
-#     return carousel_desc_list[0]
+app.clientside_callback(
+    """
+    (switchOn) => {
+       document.documentElement.setAttribute("data-bs-theme", switchOn ? "light" : "dark"); 
+       return window.dash_clientside.no_update
+    }
+    """,
+    Output("switch", "id"),
+    Input("switch", "value"),
+)
 
 @dash.callback(
     Output('hnp_text', 'children'),
@@ -55,6 +64,18 @@ def update_hnpcarousel_text(indeks):
     if indeks:
         return carousel_desc_list[indeks]
     return carousel_desc_list[0]
+
+
+@dash.callback(
+    Output('demo_text', 'children'),
+    Input('demo_carousel', 'active_index')
+)
+def update_democarousel_text(indeks):
+    if indeks:
+        return demo_desc_list[indeks]
+    return demo_desc_list[0]
+
+
 # if __name__ == "__main__":
-app.run_server(port=8050, host='0.0.0.0')
+app.run_server(debug=False, use_reloader=False)
 
